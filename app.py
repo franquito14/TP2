ed9f917ea311288e4080f16ef6f312cdbbc581f7
franquito14 <141892698+franquito14@users.noreply.github.com>
2024-05-18 17:55:08 -0300

    Endpoints adicionales y APIrest que recibe JSon en post

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+    
+@app.route('/goodbye')
+def goodbye():
+    return 'Goodbye, World!'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
+
+@app.route('/api/data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    return jsonify({"message": "Data received", "data": data})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
